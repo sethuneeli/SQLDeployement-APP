d0e519984bb60e256d5e92057adf7ec10718d7df
sethun <sethun@marketamerica.com>
2025-10-15 13:00:16 -0400

    Implementation script applied to TEST

---
+++ scripts/2025-10-15T17-00-16-124Z_implementation_TEST.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[TableEmp11] (
+  [Id] int NOT NULL,
+  [Name] nvarchar(100) NULL,
+  [Salary] decimal(10,2) NULL
+);
+ALTER TABLE [dbo].[TableEmp11] ADD CONSTRAINT [PK__TableEmp__3214EC077AB0E889] PRIMARY KEY ([Id]);
+GO
