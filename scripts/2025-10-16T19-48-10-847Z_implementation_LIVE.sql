b1f46f9219a25dd00087c6b940f2498006d2b84c
sethun <sethun@marketamerica.com>
2025-10-16 15:48:11 -0400

    Implementation script applied to LIVE
    Environment: LIVE
    
    User: system
    
    Timestamp: 2025-10-16T19:48:10.990Z
    
    Objects: MA.Customer, MA.Customer_view, MA.Customer_proc
    
    Action: implementation

---
+++ scripts/2025-10-16T19-48-10-847Z_implementation_LIVE.sql
@@ -0,0 +1,17 @@
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = N'MA') EXEC('CREATE SCHEMA [MA]');
+GO
+
+CREATE TABLE [MA].[Customer] (
+  [ID] int NULL,
+  [name] nvarchar(10) NULL,
+  [sal] int NULL,
+  [sal1] int NULL
+);
+
+GO
+
+create view [MA].Customer_view as select name from [MA].[Customer];
+GO
+
+create proc [MA].Customer_proc as select name from [MA].[Customer];
+GO
